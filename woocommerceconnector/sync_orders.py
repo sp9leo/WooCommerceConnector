20c7bfda565bc7fcc39a54ac7f15f0ab0a337176
sp9leo <sp9leonardo@gmail.com>
2020-12-02 20:09:12 +0100

    Update sync_orders.py

--- woocommerceconnector/sync_orders.py
+++ woocommerceconnector/sync_orders.py
@@ -130,6 +130,7 @@ def create_new_customer_of_guest(woocommerce_order):
             "woocommerce_customer_id": cust_id,
             "sync_with_woocommerce": 0,
             "customer_group": woocommerce_settings.customer_group,
+            "territory":"Slovenija",
             #"territory": frappe.utils.nestedset.get_root_of("Territory"),
             "customer_type": _("Individual")
         })
